bbd1c8bf4f54ebf4ecfab33bfb22260136ec558c
! taz <tazisadev@gmail.com>
2026-01-03 03:29:55 +0000

    pushed

--- Main/variables.py
+++
@@ -1,6 +0,0 @@
-TOKEN = "MTQ1NTU2NTk3MDc4NDUxODI3OA.G6rGaG.-OgMyYQ8iTtazvYbX2e9Zt5gsjTzFn6Igm8t6Q"
-GUILD_ID = 1454158368850706525  # ton serveur
-
-import discord
-from discord.ext import commands
-import asyncio
