af341375bd779b958ef1f1a4bcb8dbd2f5e91bb0
PeriodicBrake63 <125907242+PeriodicBrake63@users.noreply.github.com>
2026-01-08 19:11:42 +0000

    Import fix n°3

--- Main/main.py
+++ Main/main.py
@@ -1,7 +1,7 @@
 import os
 import json
 import variables as ENV_V
-from .Commands.Client import client
-from .Commands.Trade import *
+from Commands.Client import client
+from Commands.Trade import *
 
 client.run(ENV_V.TOKEN)
