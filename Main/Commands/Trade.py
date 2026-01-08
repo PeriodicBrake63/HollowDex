b16ef0ab666dbd74eae748a90bd8899ff95fb7a0
PeriodicBrake63 <125907242+PeriodicBrake63@users.noreply.github.com>
2026-01-08 19:25:47 +0000

    import fix n°8

--- Main/Commands/Trade.py
+++ Main/Commands/Trade.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import asyncio
 import json
 from .Client import client
-import Hautocompletion as ac
+from .autocompletion import user_autocomplete, enemy_autocomplete
 
 @client.tree.command(
     name="trade begin",
@@ -11,6 +11,9 @@ import Hautocompletion as ac
 )
 @discord.app_commands.describe(
     usr="user you want to trade with"
+)
+@discord.app_commands.autocomplete(
+    usr=user_autocomplete()
 )
 async def trade_begin(interaction: discord.Interaction, usr: discord.user):
     await interaction.response.send_message(f"trade started with {usr.name}")
@@ -23,7 +26,7 @@ async def trade_begin(interaction: discord.Interaction, usr: discord.user):
     enemy="enemy you want to add to the ongoing trade"
 )
 @discord.app_commands.autocomplete(
-    enemy=ac.enemy_autocomplete()
+    enemy=enemy_autocomplete()
 )
 async def trade_add(interaction: discord.Interaction, enemy: str):
     await interaction.response.send_message(f"enemy '{enemy}' added to the ongoing trade")
