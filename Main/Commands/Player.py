65e4ff6a4953f68a36511985b2f02fea1384b762
PeriodicBrake63 <125907242+PeriodicBrake63@users.noreply.github.com>
2026-01-09 09:05:36 +0000

    added basic player commands

---
+++ Main/Commands/Player.py
@@ -0,0 +1,22 @@
+import discord
+import asyncio
+from .Client import client
+from discord import app_commands
+
+player = app_commands.Group(name="player", description="Player commands")
+
+@player.command(
+    name="init",
+    description="Initializes a player as a collector"
+)
+async def player_init(interaction: discord.Interaction, usr: discord.User):
+    await interaction.response.send_message(f"*act like your account in initialized while it's really not in database*")
+
+@player.command(
+    name="config",
+    description="opens the config panel"
+)
+async def player_config(interaction: discord.Interaction, enemy: str):
+    await interaction.response.send_message(f"*act like this opened the config panel*")
+
+client.tree.add_command(player)
--- Main/main.py
+++ Main/main.py
@@ -3,5 +3,11 @@ import json
 import variables as ENV_V
 from Commands.Client import client
 from Commands.Trade import *
+from Commands.Player import *
+
+@client.event
+async def on_ready():
+    await client.tree.sync()
+    print(f'Bot is online as {client.user}')       
 
 client.run(ENV_V.TOKEN)
