b8d67e66bdd63be961a78814898e54f2ea7cc111
PeriodicBrake63 <125907242+PeriodicBrake63@users.noreply.github.com>
2026-01-09 17:07:11 +0000

    added test server to the server database

--- Main/Commands/Player.py
+++ Main/Commands/Player.py
@@ -10,13 +10,13 @@ player = app_commands.Group(name="player", description="Player commands")
     description="Initializes a player as a collector"
 )
 async def player_init(interaction: discord.Interaction, usr: discord.User):
-    await interaction.response.send_message(f"*act like your account in initialized while it's really not in database*")
+    await interaction.response.send_message(f"*act like your account in initialized while it's really not in database*", ephemeral=True)
 
 @player.command(
     name="config",
     description="opens the config panel"
 )
 async def player_config(interaction: discord.Interaction, enemy: str):
-    await interaction.response.send_message(f"*act like this opened the config panel*")
+    await interaction.response.send_message(f"*act like this opened the config panel*", ephemeral=True)
 
 client.tree.add_command(player)
