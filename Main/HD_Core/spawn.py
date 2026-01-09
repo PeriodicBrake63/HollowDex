3c2d9a7a612f3d56a29501b76b7d52001b8a3d7b
PeriodicBrake63 <125907242+PeriodicBrake63@users.noreply.github.com>
2026-01-09 19:21:15 +0000

    feat: enhance enemy spawning logic with rarity-based selection

--- Main/HD_Core/spawn.py
+++ Main/HD_Core/spawn.py
@@ -10,10 +10,20 @@ import json
 with open("/home/container/Main/DATABASE/ServerBase.json", "r") as f:
     server_base = json.load(f)
 
+with open("/home/container/Main/DATABASE/Enemylist.json", "r") as f:
+    enemy_list = json.load(f)
+
 @client.event
 async def on_message(message: discord.Message):
     if message.author == client.user:
         return
     else:
         if random.randint(1, 100) <= 5:
-            await message.guild.get_channel(server_base[str(message.guild.id)]["spawn_channel_id"]).send("You feel a sudden chill run down your spine...")
+            items = list(enemy_list.values())
+            weights = [1 / item["rarity"] for item in items]
+
+            chosen = random.choices(items, weights=weights, k=1)[0]
+            path = chosen["path"]
+            await message.guild.get_channel(server_base[str(message.guild.id)]["spawn_channel_id"]).send(
+                embed=discord.Embed().set_image(url="https://i.imgur.com/3ZQ3Z6L.png")
+            )
