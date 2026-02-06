b7830b1f2aea0a487bff6aac66046a5b20111b12
Periodic Brake <periodicbrake63@gmail.com>
2026-02-06 21:50:30 +0100

    added base paths as client variables for easier access

--- Main/HD_Core/Commands/Client.py
+++ Main/HD_Core/Commands/Client.py
@@ -10,17 +10,17 @@ class MyClient(discord.Client):
         intents.message_content = True
         super().__init__(intents=intents)
         self.tree = discord.app_commands.CommandTree(self)
-        BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
-        DB_DIR = os.path.join(BASE_DIR, "DATABASE")
+        self.BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+        self.DB_DIR = os.path.join(self.BASE_DIR, "DATABASE")
 
         self.ServerBase = {}
         self.PlayerBase = {}
 
-        with open(os.path.join(DB_DIR, "ServerBase.json"), "r") as f:
+        with open(os.path.join(self.DB_DIR, "ServerBase.json"), "r") as f:
             self.ServerBase = json.load(f)
-        with open(os.path.join(DB_DIR, "PlayerBase.json"), "r") as f:
+        with open(os.path.join(self.DB_DIR, "PlayerBase.json"), "r") as f:
             self.PlayerBase = json.load(f)
-        with open(os.path.join(DB_DIR, "Enemylist.json"), "r") as f:
+        with open(os.path.join(self.DB_DIR, "Enemylist.json"), "r") as f:
             self.EnemyList = json.load(f)
 
     async def setup_hook(self):
